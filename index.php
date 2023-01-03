6c158b7cf7291bcc85ca09a82e7b563820dc89f2
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-01-03 12:05:06 +0100

    front ctrlr V $_GET['page'] ET $_SERVER['REQUEST_URI']

--- index.php
+++ index.php
@@ -1,33 +1,34 @@
 <?php
 
-/*
-switch ($_SERVER['REQUEST_URI']) {
-    case '/page_un':
-        include 'pages/chat.php';
-        break;
-    case '/page_deux':
-        include 'pages/chien.php';
-        break;
-    case '/page_trois':
-        include 'pages/tortue.php';
-        break;
-    default:
-        include 'pages/home.php';
-        break;
+if (isset($_GET["page"])) {
+    switch ($_GET["page"]) {
+        case 'chat':
+            include 'pages/chat.php';
+            break;
+        case 'chien':
+            include 'pages/chien.php';
+            break;
+        case 'tortue':
+            include 'pages/tortue.php';
+            break;
+        default:
+            include 'pages/404.php';
+            break;
+    }
+} else {
+    switch ($_SERVER['REQUEST_URI']) {
+        case '/chat':
+            include 'pages/chat.php';
+            break;
+        case '/chien':
+            include 'pages/chien.php';
+            break;
+        case '/tortue':
+            include 'pages/tortue.php';
+            break;
+        default:
+            include 'pages/home.php';
+            break;
+    }
 }
-*/
 
-switch ($_GET["page"]) {
-    case 'chat':
-        include 'pages/chat.php';
-        break;
-    case 'chien':
-        include 'pages/chien.php';
-        break;
-    case 'tortue':
-        include 'pages/tortue.php';
-        break;
-    default:
-        include 'pages/home.php';
-        break;
-}
---
+++ pages/404.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html lang="en">
+
+
+<?php
+$title = "pas trouvé";
+$nav = $title;
+require 'components/header.php';
+// include opening body
+?>
+
+<main>
+    <H1>404</H1>
+</main>
+
+
+<?php
+require 'components/footer.php';
+// include closing body
+?>
+
+
+</html>
