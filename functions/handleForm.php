e24e6ca012c81511a56b96027f565f024347118a
AB <aurelien.bouchard@le-campus-numeric.fr>
2023-01-04 12:02:47 +0100

    affichage des erreurs + persistance des données

--- functions/handleForm.php
+++ functions/handleForm.php
@@ -4,17 +4,28 @@
 
 $form_is_valid = false;
 $data = "";
-$errors = [];
+if (!isset($errors)) {
+    $errors = [];
+}
 
 
 $validationSchema = [
+    // to passive inputs : htmlentities($input)
     'civilite' => [FILTER_VALIDATE_REGEXP, ["regexp"=>"/(^M\.$|^Mme$)/"]],
     'lastname' => [FILTER_VALIDATE_REGEXP, ["regexp"=>"/^[a-zA-Z-']{2,30}$/"]],
     'firstname' => [FILTER_VALIDATE_REGEXP, ["regexp"=>"/^[a-zA-Z-']{2,30}$/"]],
     'email' => [FILTER_VALIDATE_EMAIL, null],
     'raison' => [FILTER_VALIDATE_REGEXP, ["regexp"=>"/(^job$|^presta$|^info$)/"]],
-    'message' => [FILTER_VALIDATE_REGEXP, ["regexp"=>"/^.{15,500}$/"]],
-    'integer' => [FILTER_VALIDATE_INT, null]
+    'message' => [FILTER_VALIDATE_REGEXP, ["regexp"=>"/^.{15,500}$/"]]
+];
+
+$errorMessages = [
+    'civilite' => "Spécifiez la civilité",
+    'lastname' => "Entrez votre nom de famille",
+    'firstname' => "Entrez votre prénom",
+    'email' => "Votre email ne semble pas valide",
+    'raison' => "Indiquez la raison de votre message",
+    'message' => "Veuillez laisser un message entre 15 et 500 caractères"
 ];
 
 
@@ -24,21 +35,21 @@ if (count($_POST) === 6 ) {
 
     // check received data validity
     foreach ($validationSchema as $key => $value) {
-
         if (filter_input(INPUT_POST, $key, $value[0], $value[1] ? array("options"=>$value[1]) : null )) {
             $data .= $key . " : " . $_POST[$key] . "\n";
         } else {
-            $data .= "error ! \n";
+            $form_is_valid = false;
+            array_push($errors, $errorMessages[$key]);
         }
     }
 }
 
 
 if ($form_is_valid) {
-    echo "ok";
+    //echo "ok";
     $file = "messages" . DIRECTORY_SEPARATOR . $_POST['firstname'] ."_". $_POST['lastname'] ."_". date('Y-m-d-H-i-s') . ".txt";
     file_put_contents($file, $data, FILE_APPEND);
 } else {
-    //echo "fock";
+    //echo "Le formulaire n'était pas valide :(";
 }
 
--- pages/contact.php
+++ pages/contact.php
@@ -9,57 +9,76 @@ require 'components/header.php';
 // include opening body
 ?>
 
+
+<?php
+$errors = [];
+require "functions/handleForm.php" ?>
+
+
 <main>
     <H1>Formulaire de contact</H1>
 
     <div class="container">
-        <form class="border p-3 rounded form" action="/?page=contact" method="POST">
-            <div class="my-2">
+        <form class="form-group p-1 rounded" action="/?page=contact" method="POST">
+
+            <!-- show list of errors -->
+            <?php if (count($errors)) {
+                foreach ($errors as $key => $err) {
+                    ?>
+                    <div class="alert alert-danger">
+                        <?php echo $err; ?>
+                    </div>
+                    <?php
+                }
+            }?>
+            <div class="my-2 form-control">
                 <label for="civilite">Civilité :</label>
                 <select name="civilite">
-                    <option value="M." >M.</option>
-                    <option value="Mme" >Mme</option>
+                    <option value="M."  <?php if (htmlentities($_POST['civilite']) ==='M.') { echo "selected"; } ?> >M.</option>
+                    <option value="Mme" <?php if (htmlentities($_POST['civilite']) ==='Mme') { echo "selected"; } ?> >Mme</option>
                 </select>
             </div>
 
-            <div class="my-2">
+            <div class="my-2 form-control">
                 <label for="lastname">Nom :</label>
-                <input type="text" name="lastname">
+                <input type="text" name="lastname" value="<?php echo htmlentities($_POST['lastname']); ?>">
             </div>
 
-            <div class="my-2">
+            <div class="my-2 form-control">
                 <label for="firstname">Prénom :</label>
-                <input type="text" name="firstname">
+                <input type="text" name="firstname" value="<?php echo htmlentities($_POST['firstname']); ?>">
             </div>
 
-            <div class="my-2">
+            <div class="my-2 form-control">
                 <label for="email">Email :</label>
-                <input type="email" name="email">
+                <input type="email" name="email" value="<?php echo htmlentities($_POST['email']); ?>">
             </div>
 
-            <div class="my-2">
+            <div class="my-2 form-control">
                 <fieldset name="raison">
                     <div class="mb-2">
                         <label for="job">Proposition d'emploi</label>
-                        <input type="radio" id="job" name="raison" value="job">
+                        <input type="radio" id="job" name="raison" value="job" <?php if (htmlentities($_POST['raison']) ==='job') { echo "checked"; } ?> >
                     </div>
 
                     <div class="mb-2">
                         <label for="info">Demande d'infos</label>
-                        <input type="radio" id="info" name="raison" value="info">
+                        <input type="radio" id="info" name="raison" value="info" <?php if (htmlentities($_POST['raison']) ==='info') { echo "checked"; } ?> >
                     </div>
 
 
                     <label for="presta">Prestation</label>
-                    <input type="radio" id="presta" name="raison" value="presta">
+                    <input type="radio" id="presta" name="raison" value="presta" <?php if (htmlentities($_POST['raison']) ==='presta') { echo "checked"; } ?> >
                 </fieldset>
             </div>
 
-            <div class="my-2">
+            <div class="my-2 form-control">
                 <p>
                     <label for="message">Message :</label>
                 </p>
-                <textarea name="message" id="message" rows="4" ></textarea>
+                <textarea name="message" id="message" rows="4"
+                          placeholder="Votre message ici"
+                ><?php echo htmlentities($_POST['message']); ?></textarea>
             </div>
 
             <button type="submit" class="btn btn-primary" >Envoyer</button>
@@ -67,12 +86,10 @@ require 'components/header.php';
         </form>
 
 
-        <?php require "functions/handleForm.php" ?>
-
-        <pre>
-            <?php print_r($_GET); ?>
-            <?php print_r($_POST); ?>
-        </pre>
+<!--        <pre>
+            <?php /*print_r($_GET); */?>
+            <?php /*print_r($_POST); */?>
+        </pre>-->
     </div>
 </main>
 
